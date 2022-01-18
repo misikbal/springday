492edf8cb8029583e45fffa27da2f0f4a252cf7b
Muhamemd ikbal sirdas <ikbal.sirdas@gmail.com>
2022-01-18 21:36:29 +0300

    nodejs-web

--- routes/shop.js
+++ routes/shop.js
@@ -24,31 +24,31 @@ const myCache = new NodeCache( { stdTTL: 300, checkperiod: 310 } );
 router.get("/",
     
 locals,home,isMainMode,
-function (req, res, next) {
-    if(!req.session.isAuthenticated){
-            if (req.method != 'GET') {
-                return next();
-        }
-        var cachedReponse = myCache.get(req.url);
-        if (cachedReponse) {
-        res.header(cachedReponse.headers);
-        res.header('X-Proxy-Cache', 'HIT');
-        return res.send(cachedReponse.body);
-        } else {
-        res.originalSend = res.send;
-        res.send = (body) => {
-            myCache.set(req.url, {
-            'headers'   : res.getHeaders(),
-            'body'      : body
-            });
-            res.header('X-Proxy-Cache', 'MISS');
-            res.originalSend(body);
-        };
-        return next();
-        }
-    }
-    next();
-},
+// function (req, res, next) {
+//     if(!req.session.isAuthenticated){
+//             if (req.method != 'GET') {
+//                 return next();
+//         }
+//         var cachedReponse = myCache.get(req.url);
+//         if (cachedReponse) {
+//         res.header(cachedReponse.headers);
+//         res.header('X-Proxy-Cache', 'HIT');
+//         return res.send(cachedReponse.body);
+//         } else {
+//         res.originalSend = res.send;
+//         res.send = (body) => {
+//             myCache.set(req.url, {
+//             'headers'   : res.getHeaders(),
+//             'body'      : body
+//             });
+//             res.header('X-Proxy-Cache', 'MISS');
+//             res.originalSend(body);
+//         };
+//         return next();
+//         }
+//     }
+//     next();
+// },
 
 shopController.getIndex);
 
