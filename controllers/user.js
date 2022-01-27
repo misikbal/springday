b22a7fe2d86fa5bbf66eed166820a4b3dbd2893c
Muhamemd ikbal sirdas <ikbal.sirdas@gmail.com>
2022-01-27 13:59:05 +0300

    nodejs-web

--- controllers/user.js
+++ controllers/user.js
@@ -90,9 +90,9 @@ exports.postAddSlide = async(req, res, next) => {
     
     const resizedImageBuf=await sharp(req.files.slideimg[0].path)
     .resize(540)
-    .webp({quality:10,alphaQuality:10,lossless:false,progressive:true})
-    .jpeg({quality:10,alphaQuality:10,lossless:false,progressive:true})
-    .png({quality:10,alphaQuality:10,lossless:false,progressive:true})
+    .webp({quality:10,alphaQuality:10,lossless:true,progressive:true})
+    .jpeg({quality:10,alphaQuality:10,lossless:true,progressive:true})
+    .png({quality:10,alphaQuality:10,lossless:true,progressive:true})
     .toBuffer()
     // await writeFileAsync(req.files.slideimg[0].destination+"/"+req.files.slideimg[0].filename.toString().split(".webp")[0]+".txt", "data:"+req.files.slideimg[0].mimetype+";base64,"+resizedImageBuf.toString('base64'), 'utf-8');
     const slide = new Post(
@@ -221,18 +221,18 @@ exports.postEditSlide = async (req, res, next) => {
                 .webp({quality:10,alphaQuality:10,lossless:true,progressive:true})
                 .jpeg({quality:10,alphaQuality:10,lossless:true,progressive:true})
                 .png({quality:10,alphaQuality:10,lossless:true,progressive:true})
-                // .toBuffer()
+                .toBuffer()
                 // await writeFileAsync(req.files.slideimg[0].destination+"/"+req.files.slideimg[0].filename.toString().split(".webp")[0]+".txt", "data:"+req.files.slideimg[0].mimetype+";base64,"+resizedImageBuf.toString('base64'), 'utf-8');
-                .toFile(
-                    path.resolve(
-                    req.files.slideimg[0].destination,
-                    "resized",
-                    req.files.slideimg[0].filename
-                    )
-                );
-                fs.unlinkSync(req.files.slideimg[0].path);
-                // slide.slide.image =  "data:"+req.files.slideimg[0].mimetype+";base64,"+resizedImageBuf.toString('base64');
-                slide.slide.image=req.files.slideimg[0].filename;
+                // .toFile(
+                //     path.resolve(
+                //     req.files.slideimg[0].destination,
+                //     "resized",
+                //     req.files.slideimg[0].filename
+                //     )
+                // );
+                // fs.unlinkSync(req.files.slideimg[0].path);
+                slide.slide.image =  "data:"+req.files.slideimg[0].mimetype+";base64,"+resizedImageBuf.toString('base64');
+                // slide.slide.image=req.files.slideimg[0].filename;
             }
             const progress=new Process(
                 { 
